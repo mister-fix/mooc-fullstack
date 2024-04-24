e6e9a40976ed9692a120c3dfdbaf6db076ff0136
Stephen <hellostephenwm@gmail.com>
2024-04-24 19:29:13 -0400

    Covers exercises 1.12 - 1.14

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+
+const Header = (props) => {
+	return <h1>{props.text}</h1>;
+};
+
+const Button = (props) => {
+	return <button onClick={props.eventHandler}>{props.text}</button>;
+};
+
+const Anecdote = (props) => {
+	return (
+		<>
+			<p>{props.text}</p>
+			<p>has {props.votes} votes</p>
+		</>
+	);
+};
+
+const App = () => {
+	const anecdotes = [
+		"If it hurts, do it more often.",
+		"Adding manpower to a late software project makes it later!",
+		"The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+		"Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+		"Premature optimization is the root of all evil.",
+		"Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+		"Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+		"The only way to go fast, is to go well.",
+	];
+
+	const [selected, setSelected] = useState(0);
+	const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+	const highestVotedIndex = votes.indexOf(Math.max(...votes));
+
+	const handleAnecdoteSelection = () => {
+		// Generating random number where maximum value is the length of the anecdotes array
+		const randomIndex = Math.floor(Math.random() * anecdotes.length);
+		// Setting randomly generated number to the selected variable
+		setSelected(randomIndex);
+	};
+
+	const handleVote = () => {
+		// Making a copy of the votes array
+		const newVotes = [...votes];
+		// Incrementing the copied votes array at the selected anecdote index by 1
+		newVotes[selected] += 1;
+		// Updating the votes
+		setVotes(newVotes);
+	};
+
+	return (
+		<div>
+			<div>
+				<Header text="Anecdote of the day" />
+
+				<Anecdote
+					text={anecdotes[selected]}
+					votes={votes[selected]}
+				/>
+
+				<div>
+					<Button
+						eventHandler={handleVote}
+						text="vote"
+					/>
+					<Button
+						eventHandler={handleAnecdoteSelection}
+						text="next anecdote"
+					/>
+				</div>
+			</div>
+
+			<div>
+				<Header text="Anecdote with most votes" />
+
+				<Anecdote
+					text={anecdotes[highestVotedIndex]}
+					votes={votes[highestVotedIndex]}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default App;
