bdd4fa84d960b53071385a6ae047e8861ddd37c2
Stephen <hellostephenwm@gmail.com>
2024-05-26 22:42:53 -0400

    added data validation to schema

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -16,8 +16,21 @@ mongoose
 	});
 
 const personSchema = new mongoose.Schema({
-	name: String,
-	number: String,
+	name: {
+		type: String,
+		minLength: 3,
+		required: true,
+	},
+	number: {
+		type: String,
+		validate: {
+			validator: (v) => {
+				return /\d{2,3}-\d+$/.test(v) && v.length >= 8;
+			},
+			message: (props) => `${props.value} is not a valid phone number`,
+		},
+		required: [true, "Phone number required"],
+	},
 });
 
 personSchema.set("toJSON", {
