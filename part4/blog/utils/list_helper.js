79cedb9f641c4e985e701b479c24485080d3acb9
Stephen <hellostephenwm@gmail.com>
2024-05-31 19:16:27 -0400

    file with functions to be tested

---
+++ part4/blog/utils/list_helper.js
@@ -0,0 +1,63 @@
+const dummy = (blogs) => {
+	return 1;
+};
+
+const totalLikes = (blogs) => {
+	return blogs.length === 0
+		? null
+		: blogs.reduce((sum, blog) => sum + blog.likes, 0);
+};
+
+const favoriteBlog = (blogs) => {
+	return blogs.length === 0
+		? null
+		: blogs.reduce((max, blog) => (blog.likes > max.likes ? blog : max));
+};
+
+const mostBlogs = (blogs) => {
+	if (blogs.length === 0) {
+		return null;
+	}
+
+	const authorBlogCount = blogs.reduce((count, blog) => {
+		count[blog.author] = (count[blog.author] || 0) + 1;
+		return count;
+	}, {});
+
+	const topAuthor = Object.keys(authorBlogCount).reduce((max, author) => {
+		return authorBlogCount[author] > authorBlogCount[max] ? author : max;
+	});
+
+	return {
+		author: topAuthor,
+		blogs: authorBlogCount[topAuthor],
+	};
+};
+
+const mostLikes = (blogs) => {
+	if (blogs.length === 0) {
+		return null;
+	}
+
+	const authorLikesCount = blogs.reduce((count, blog) => {
+		count[blog.author] = (count[blog.author] || 0) + blog.likes;
+		return count;
+	}, {});
+
+	const topAuthor = Object.keys(authorLikesCount).reduce((max, author) => {
+		return authorLikesCount[author] > authorLikesCount[max] ? author : max;
+	});
+
+	return {
+		author: topAuthor,
+		likes: authorLikesCount[topAuthor],
+	};
+};
+
+module.exports = {
+	dummy,
+	totalLikes,
+	favoriteBlog,
+	mostBlogs,
+	mostLikes,
+};
