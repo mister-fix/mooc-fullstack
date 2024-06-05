d47be121c519a5e317635905138e109ad5fe0e8a
Stephen <hellostephenwm@gmail.com>
2024-06-04 23:19:41 -0400

    refactored methods to use async/await

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,49 +1,41 @@
-const logger = require('../utils/logger')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((returnedBlogs) => {
-    response.json(returnedBlogs)
-  })
+blogsRouter.get('/api/blogs', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.get('/api/blogs/:id', (request, response) => {
-  Blog.findById(request.params.id).then((blog) => {
-    if (blog) {
-      response.json(blog)
-    } else {
-      response.status(404).end()
-    }
-  })
+blogsRouter.get('/api/blogs/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
 })
 
-blogsRouter.post('/api/blogs', (request, response) => {
+blogsRouter.post('/api/blogs', async (request, response) => {
   const body = request.body
+
+  if (!body.title || !body.url) {
+    return response.status(400).end()
+  }
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes,
+    likes: body.likes || 0,
   })
 
-  blog
-    .save()
-    .then((savedBlog) => {
-      response.json(savedBlog)
-    })
-    .catch((error) => logger.error('error saving blog:', error.message))
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/api/blogs/:id', (request, response) => {
-  Blog.findByIdAndDelete(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch((error) => {
-      logger.error('error deleting blog:', error.message)
-    })
+blogsRouter.delete('/api/blogs/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = blogsRouter
