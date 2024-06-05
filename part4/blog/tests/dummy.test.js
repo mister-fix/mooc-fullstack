7d005f7b2983f1d1eb3727971d2f0474ffca7f5b
Stephen <hellostephenwm@gmail.com>
2024-06-04 23:21:01 -0400

    linted file

--- part4/blog/tests/dummy.test.js
+++ part4/blog/tests/dummy.test.js
@@ -1,12 +1,12 @@
-const { test, describe } = require("node:test");
-const assert = require("node:assert");
-const listHelper = require("../utils/list_helper");
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+const listHelper = require('../utils/list_helper')
 
-describe("dummy", () => {
-	test("dummy returns one", () => {
-		const blogs = [];
+describe('dummy', () => {
+  test('dummy returns one', () => {
+    const blogs = []
 
-		const result = listHelper.dummy(blogs);
-		assert.strictEqual(result, 1);
-	});
-});
+    const result = listHelper.dummy(blogs)
+    assert.strictEqual(result, 1)
+  })
+})
