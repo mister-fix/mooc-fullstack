c2a17418d6cc788eb8592e5191b4b88daf330203
Stephen <hellostephenwm@gmail.com>
2024-06-04 23:20:28 -0400

    linted file

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,63 +1,64 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
-	return 1;
-};
+  return 1
+}
 
 const totalLikes = (blogs) => {
-	return blogs.length === 0
-		? null
-		: blogs.reduce((sum, blog) => sum + blog.likes, 0);
-};
+  return blogs.length === 0
+    ? null
+    : blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
 
 const favoriteBlog = (blogs) => {
-	return blogs.length === 0
-		? null
-		: blogs.reduce((max, blog) => (blog.likes > max.likes ? blog : max));
-};
+  return blogs.length === 0
+    ? null
+    : blogs.reduce((max, blog) => (blog.likes > max.likes ? blog : max))
+}
 
 const mostBlogs = (blogs) => {
-	if (blogs.length === 0) {
-		return null;
-	}
+  if (blogs.length === 0) {
+    return null
+  }
 
-	const authorBlogCount = blogs.reduce((count, blog) => {
-		count[blog.author] = (count[blog.author] || 0) + 1;
-		return count;
-	}, {});
+  const authorBlogCount = blogs.reduce((count, blog) => {
+    count[blog.author] = (count[blog.author] || 0) + 1
+    return count
+  }, {})
 
-	const topAuthor = Object.keys(authorBlogCount).reduce((max, author) => {
-		return authorBlogCount[author] > authorBlogCount[max] ? author : max;
-	});
+  const topAuthor = Object.keys(authorBlogCount).reduce((max, author) => {
+    return authorBlogCount[author] > authorBlogCount[max] ? author : max
+  })
 
-	return {
-		author: topAuthor,
-		blogs: authorBlogCount[topAuthor],
-	};
-};
+  return {
+    author: topAuthor,
+    blogs: authorBlogCount[topAuthor],
+  }
+}
 
 const mostLikes = (blogs) => {
-	if (blogs.length === 0) {
-		return null;
-	}
+  if (blogs.length === 0) {
+    return null
+  }
 
-	const authorLikesCount = blogs.reduce((count, blog) => {
-		count[blog.author] = (count[blog.author] || 0) + blog.likes;
-		return count;
-	}, {});
+  const authorLikesCount = blogs.reduce((count, blog) => {
+    count[blog.author] = (count[blog.author] || 0) + blog.likes
+    return count
+  }, {})
 
-	const topAuthor = Object.keys(authorLikesCount).reduce((max, author) => {
-		return authorLikesCount[author] > authorLikesCount[max] ? author : max;
-	});
+  const topAuthor = Object.keys(authorLikesCount).reduce((max, author) => {
+    return authorLikesCount[author] > authorLikesCount[max] ? author : max
+  })
 
-	return {
-		author: topAuthor,
-		likes: authorLikesCount[topAuthor],
-	};
-};
+  return {
+    author: topAuthor,
+    likes: authorLikesCount[topAuthor],
+  }
+}
 
 module.exports = {
-	dummy,
-	totalLikes,
-	favoriteBlog,
-	mostBlogs,
-	mostLikes,
-};
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
+}
