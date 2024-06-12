ed5a22744d6f3ee4f28f0f313deca0a30627c946
Stephen <hellostephenwm@gmail.com>
2024-06-12 15:25:55 -0400

    enabled user extractor middleware to work with blog endpoint

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -6,6 +6,8 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 mongoose.set('strictQuery', false)
@@ -24,7 +26,11 @@ mongoose
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/blogs', blogsRouter)
+app.use(middleware.tokenExtractor)
+
+app.use('/api/login', loginRouter)
+app.use('/api/blogs', middleware.userExtractor, blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
