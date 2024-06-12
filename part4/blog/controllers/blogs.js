1c1501ec74b3355ef514037da2250dcd4d366eac
Stephen <hellostephenwm@gmail.com>
2024-06-12 15:26:26 -0400

    refactored post and delete functions to make use of token extractor and user extractor middleware

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -2,7 +2,7 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
@@ -18,6 +18,12 @@ blogsRouter.get('/:id', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
+  if (!request.token) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const user = request.user
+
   if (!body.title || !body.url) {
     return response.status(400).end()
   }
@@ -27,9 +33,13 @@ blogsRouter.post('/', async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
+    user: user.id,
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
@@ -50,8 +60,19 @@ blogsRouter.put('/:id', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id)
-  response.status(204).end()
+  const user = request.user
+  const blog = await Blog.findById(request.params.id)
+
+  if (!blog) {
+    return response.status(404).json({ error: 'blog not found' })
+  }
+
+  if (user && user._id.toString() === blog.user.toString()) {
+    await Blog.deleteOne({ _id: request.params.id })
+    response.status(204).end()
+  } else {
+    return response.status(401).json({ error: 'unauthorized' })
+  }
 })
 
 module.exports = blogsRouter
