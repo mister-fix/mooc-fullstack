92b319755d84e1a705ab28d64f6677686fff417f
Stephen <hellostephenwm@gmail.com>
2024-06-20 20:11:52 -0400

    added blog update method

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -17,13 +17,18 @@ const getAll = () => {
 
 const create = async (newObject) => {
 	const config = {
-		headers: {
-			Authorization: token,
-		},
+		headers: { Authorization: token },
 	};
-
 	const request = axios.post(baseUrl, newObject, config);
 	return request.then((response) => response.data);
 };
 
-export default { setToken, getAll, create };
+const update = async (id, blogObject) => {
+	const config = {
+		headers: { Authorization: token },
+	};
+	const request = axios.put(`${baseUrl}/${id}`, blogObject, config);
+	return request.then((response) => response.data);
+};
+
+export default { setToken, getAll, create, update };
