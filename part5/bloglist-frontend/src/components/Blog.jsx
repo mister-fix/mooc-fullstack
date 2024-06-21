7ea4463e0a5097dd9351ce4d91cfecbad494dc02
Stephen <hellostephenwm@gmail.com>
2024-06-20 20:11:17 -0400

    refactored component, added details button

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog, handleLike }) => {
+	const [visibility, setVisibility] = useState(false);
+	const showWhenVisibile = { display: visibility ? "" : "none" };
+	const blogStyle = {
+		paddingTop: 10,
+		paddingLeft: 2,
+		border: "solid",
+		borderWidth: 1,
+		marginBottom: 5,
+	};
+
+	const toggleVisibility = () => {
+		setVisibility(!visibility);
+	};
+
+	return (
+		<div style={blogStyle}>
+			<div>
+				{blog.title} {blog.author}
+				<button onClick={toggleVisibility}>
+					{visibility ? "hide" : "view"}
+				</button>
+			</div>
+
+			<div style={showWhenVisibile}>
+				<p>{blog.url}</p>
+				<p>
+					likes {blog.likes}{" "}
+					<button onClick={(event) => handleLike(event, blog.id)}>like</button>
+				</p>
+				<p>{blog.user.name}</p>
+			</div>
+		</div>
+	);
+};
+
+export default Blog;
