74e395edf8b0bd5eef6b704925f356b8f5a0c23b
Stephen <hellostephenwm@gmail.com>
2024-06-23 01:08:10 -0400

    defined component prop-types and display name

--- part5/bloglist-frontend/src/components/Togglable.jsx
+++ part5/bloglist-frontend/src/components/Togglable.jsx
@@ -1,4 +1,5 @@
 import { useState, forwardRef, useImperativeHandle } from "react";
+import PropTypes from "prop-types";
 
 const Togglable = forwardRef((props, refs) => {
 	const [visibility, setVisibility] = useState(false);
@@ -27,5 +28,11 @@ const Togglable = forwardRef((props, refs) => {
 		</div>
 	);
 });
+
+Togglable.displayName = "Togglable";
+
+Togglable.propTypes = {
+	buttonLabel: PropTypes.string.isRequired,
+};
 
 export default Togglable;
