ef8686c43958d461daa73a68921aa0eaa301bd8f
Stephen <hellostephenwm@gmail.com>
2024-06-23 01:08:26 -0400

    linted file

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,43 +1,43 @@
-import axios from "axios";
-const baseUrl = "/api/blogs";
+import axios from 'axios'
+const baseUrl = '/api/blogs'
 
-let token = null;
+let token = null
 
 const setToken = (newToken) => {
-	token = `Bearer ${newToken}`;
-};
+  token = `Bearer ${newToken}`
+}
 
 const getAll = () => {
-	const config = {
-		headers: { Authorization: token },
-	};
-	const request = axios.get(baseUrl, config);
-	return request.then((response) => response.data);
-};
+  const config = {
+    headers: { Authorization: token },
+  }
+  const request = axios.get(baseUrl, config)
+  return request.then((response) => response.data)
+}
 
 const create = async (newObject) => {
-	const config = {
-		headers: { Authorization: token },
-	};
-	const request = axios.post(baseUrl, newObject, config);
-	return request.then((response) => response.data);
-};
+  const config = {
+    headers: { Authorization: token },
+  }
+  const request = axios.post(baseUrl, newObject, config)
+  return request.then((response) => response.data)
+}
 
 const update = async (id, blogObject) => {
-	const config = {
-		headers: { Authorization: token },
-	};
-	const request = axios.put(`${baseUrl}/${id}`, blogObject, config);
-	return request.then((response) => response.data);
-};
+  const config = {
+    headers: { Authorization: token },
+  }
+  const request = axios.put(`${baseUrl}/${id}`, blogObject, config)
+  return request.then((response) => response.data)
+}
 
 const remove = async (id) => {
-	const config = {
-		headers: { Authorization: token },
-	};
+  const config = {
+    headers: { Authorization: token },
+  }
 
-	const request = axios.delete(`${baseUrl}/${id}`, config);
-	return request.then((response) => response.data);
-};
+  const request = axios.delete(`${baseUrl}/${id}`, config)
+  return request.then((response) => response.data)
+}
 
-export default { setToken, getAll, create, update, remove };
+export default { setToken, getAll, create, update, remove }
