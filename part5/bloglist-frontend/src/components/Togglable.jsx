c95c14481a598aa0196ce7cecd24065b6f6c7baa
Stephen <hellostephenwm@gmail.com>
2024-07-11 22:39:33 -0400

    reformatted file with eslint

--- part5/bloglist-frontend/src/components/Togglable.jsx
+++ part5/bloglist-frontend/src/components/Togglable.jsx
@@ -1,38 +1,38 @@
-import { useState, forwardRef, useImperativeHandle } from "react";
-import PropTypes from "prop-types";
+import { useState, forwardRef, useImperativeHandle } from 'react'
+import PropTypes from 'prop-types'
 
 const Togglable = forwardRef((props, refs) => {
-	const [visibility, setVisibility] = useState(false);
-	const hideWhenVisible = { display: visibility ? "none" : "" };
-	const showWhenVisible = { display: visibility ? "" : "none" };
+  const [visibility, setVisibility] = useState(false)
+  const hideWhenVisible = { display: visibility ? 'none' : '' }
+  const showWhenVisible = { display: visibility ? '' : 'none' }
 
-	const toggleVisibility = () => {
-		setVisibility(!visibility);
-	};
+  const toggleVisibility = () => {
+    setVisibility(!visibility)
+  }
 
-	useImperativeHandle(refs, () => {
-		return {
-			toggleVisibility,
-		};
-	});
+  useImperativeHandle(refs, () => {
+    return {
+      toggleVisibility,
+    }
+  })
 
-	return (
-		<div style={{ marginBottom: "10px" }}>
-			<div style={hideWhenVisible}>
-				<button onClick={toggleVisibility}>{props.buttonLabel}</button>
-			</div>
-			<div style={showWhenVisible}>
-				{props.children}
-				<button onClick={toggleVisibility}>cancel</button>
-			</div>
-		</div>
-	);
-});
+  return (
+    <div style={{ marginBottom: '10px' }}>
+      <div style={hideWhenVisible}>
+        <button onClick={toggleVisibility}>{props.buttonLabel}</button>
+      </div>
+      <div style={showWhenVisible}>
+        {props.children}
+        <button onClick={toggleVisibility}>cancel</button>
+      </div>
+    </div>
+  )
+})
 
-Togglable.displayName = "Togglable";
+Togglable.displayName = 'Togglable'
 
 Togglable.propTypes = {
-	buttonLabel: PropTypes.string.isRequired,
-};
+  buttonLabel: PropTypes.string.isRequired,
+}
 
-export default Togglable;
+export default Togglable
