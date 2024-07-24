f80429e2302c7111e7d3c181df168a4f6fbee21a
Stephen <hellostephenwm@gmail.com>
2024-07-24 01:20:46 -0400

    mounted user extractor middleware

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,83 +1,84 @@
-const blogsRouter = require('express').Router()
-const Blog = require('../models/blog')
+const blogsRouter = require("express").Router();
+const { userExtractor } = require("../utils/middleware");
+const Blog = require("../models/blog");
 
-blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
-  response.json(blogs)
-})
+blogsRouter.get("/", async (request, response) => {
+	const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
+	response.json(blogs);
+});
 
-blogsRouter.get('/:id', async (request, response) => {
-  const blog = await Blog.findById(request.params.id)
-  if (blog) {
-    response.json(blog)
-  } else {
-    response.status(404).end()
-  }
-})
+blogsRouter.get("/:id", async (request, response) => {
+	const blog = await Blog.findById(request.params.id);
+	if (blog) {
+		response.json(blog);
+	} else {
+		response.status(404).end();
+	}
+});
 
-blogsRouter.post('/', async (request, response) => {
-  const body = request.body
+blogsRouter.post("/", userExtractor, async (request, response) => {
+	const body = request.body;
 
-  if (!request.token) {
-    return response.status(401).json({ error: 'token missing or invalid' })
-  }
+	if (!request.token) {
+		return response.status(401).json({ error: "token missing or invalid" });
+	}
 
-  const user = request.user
+	const user = request.user;
 
-  if (!body.title || !body.url) {
-    return response.status(400).end()
-  }
+	if (!body.title || !body.url) {
+		return response.status(400).end();
+	}
 
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes || 0,
-    user: user.id,
-  })
+	const blog = new Blog({
+		title: body.title,
+		author: body.author,
+		url: body.url,
+		likes: body.likes || 0,
+		user: user.id,
+	});
 
-  const savedBlog = await blog.save()
-  user.blogs = user.blogs.concat(savedBlog._id)
-  await user.save()
+	const savedBlog = await blog.save();
+	user.blogs = user.blogs.concat(savedBlog._id);
+	await user.save();
 
-  const populatedBlog = await savedBlog.populate('user', {
-    username: 1,
-    name: 1,
-  })
+	const populatedBlog = await savedBlog.populate("user", {
+		username: 1,
+		name: 1,
+	});
 
-  response.status(201).json(populatedBlog)
-})
+	response.status(201).json(populatedBlog);
+});
 
-blogsRouter.put('/:id', async (request, response) => {
-  const body = request.body
+blogsRouter.put("/:id", userExtractor, async (request, response) => {
+	const body = request.body;
 
-  const blog = {
-    likes: body.likes,
-  }
+	const blog = {
+		likes: body.likes,
+	};
 
-  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
-    new: true,
-    runValidators: true,
-    context: 'query',
-  }).populate('user', { username: 1, name: 1 })
+	const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+		new: true,
+		runValidators: true,
+		context: "query",
+	}).populate("user", { username: 1, name: 1 });
 
-  response.json(updatedBlog)
-})
+	response.json(updatedBlog);
+});
 
-blogsRouter.delete('/:id', async (request, response) => {
-  const user = request.user
-  const blog = await Blog.findById(request.params.id)
+blogsRouter.delete("/:id", userExtractor, async (request, response) => {
+	const user = request.user;
+	const blog = await Blog.findById(request.params.id);
 
-  if (!blog) {
-    return response.status(404).json({ error: 'blog not found' })
-  }
+	if (!blog) {
+		return response.status(404).json({ error: "blog not found" });
+	}
 
-  if (user && user._id.toString() === blog.user.toString()) {
-    await Blog.deleteOne({ _id: request.params.id })
-    response.status(204).end()
-  } else {
-    return response.status(401).json({ error: 'unauthorized' })
-  }
-})
+	if (user && user._id.toString() === blog.user.toString()) {
+		await Blog.deleteOne({ _id: request.params.id });
+		response.status(204).end();
+	} else {
+		return response.status(401).json({ error: "unauthorized" });
+	}
+});
 
-module.exports = blogsRouter
+module.exports = blogsRouter;
