0bd9e8dd6241689b97784c1d2fa427f6a5de125f
Stephen <hellostephenwm@gmail.com>
2024-07-24 00:07:26 -0400

    formatted file

--- part5/bloglist-frontend/src/components/BlogForm.jsx
+++ part5/bloglist-frontend/src/components/BlogForm.jsx
@@ -1,64 +1,64 @@
-import { useState } from "react";
-import PropTypes from "prop-types";
+import { useState } from 'react'
+import PropTypes from 'prop-types'
 
 const BlogForm = ({ createBlog }) => {
-	const [title, setTitle] = useState("");
-	const [author, setAuthor] = useState("");
-	const [url, setUrl] = useState("");
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
-	const createNewBlog = (event) => {
-		event.preventDefault();
+  const createNewBlog = (event) => {
+    event.preventDefault()
 
-		createBlog({
-			title,
-			author,
-			url,
-		});
+    createBlog({
+      title,
+      author,
+      url,
+    })
 
-		setTitle("");
-		setAuthor("");
-		setUrl("");
-	};
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+  }
 
-	return (
-		<div className="blog-form">
-			<h2>create new</h2>
-			<form onSubmit={createNewBlog}>
-				<div className="blog-title-input">
+  return (
+    <div className="blog-form">
+      <h2>create new</h2>
+      <form onSubmit={createNewBlog}>
+        <div className="blog-title-input">
 					title:
-					<input
-						data-testid="title"
-						type="text"
-						name="Title"
-						value={title}
-						onChange={({ target }) => setTitle(target.value)}
-					/>
-				</div>
-				<div className="blog-author-input">
+          <input
+            data-testid="title"
+            type="text"
+            name="Title"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div className="blog-author-input">
 					author
-					<input
-						data-testid="author"
-						type="text"
-						name="Author"
-						value={author}
-						onChange={({ target }) => setAuthor(target.value)}
-					/>
-				</div>
-				<div className="blog-url-input">
+          <input
+            data-testid="author"
+            type="text"
+            name="Author"
+            value={author}
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div className="blog-url-input">
 					url
-					<input
-						data-testid="url"
-						type="text"
-						name="URL"
-						value={url}
-						onChange={({ target }) => setUrl(target.value)}
-					/>
-				</div>
-				<button type="submit">create</button>
-			</form>
-		</div>
-	);
-};
+          <input
+            data-testid="url"
+            type="text"
+            name="URL"
+            value={url}
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
 
 // BlogForm.propTypes = {
 //   title: PropTypes.string.isRequired,
@@ -66,4 +66,4 @@ const BlogForm = ({ createBlog }) => {
 //   url: PropTypes.string.isRequired,
 // }
 
-export default BlogForm;
+export default BlogForm
