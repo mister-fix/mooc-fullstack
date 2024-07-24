b80e0dbc5871a4c93a801728291edcffe9a99b1b
Stephen <hellostephenwm@gmail.com>
2024-07-24 00:06:40 -0400

    helper module for blog app tests

---
+++ part5/bloglist-e2e/tests/helper.js
@@ -0,0 +1,14 @@
+exports.loginWith = async (page, username, password) => {
+	await page.getByTestId("username").fill(username);
+	await page.getByTestId("password").fill(password);
+	await page.getByRole("button", { name: "login" }).click();
+};
+
+exports.createBlog = async (page, content) => {
+	await page.getByRole("button", { name: "add blog" }).click();
+	await page.getByTestId("title").fill(content.title);
+	await page.getByTestId("author").fill(content.author);
+	await page.getByTestId("url").fill(content.url);
+	await page.getByRole("button", { name: "create" }).click();
+	await page.getByText(content.title).waitFor();
+};
