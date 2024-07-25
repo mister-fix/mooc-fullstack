5acc3583c868555e04863d16fcc43a4ff7b083f7
Stephen <hellostephenwm@gmail.com>
2024-07-24 22:21:00 -0400

    tests helper module

--- part5/bloglist-e2e/tests/helper.js
+++ part5/bloglist-e2e/tests/helper.js
@@ -4,11 +4,17 @@ exports.loginWith = async (page, username, password) => {
 	await page.getByRole("button", { name: "login" }).click();
 };
 
-exports.createBlog = async (page, content) => {
-	await page.getByRole("button", { name: "add blog" }).click();
-	await page.getByTestId("title").fill(content.title);
-	await page.getByTestId("author").fill(content.author);
-	await page.getByTestId("url").fill(content.url);
-	await page.getByRole("button", { name: "create" }).click();
-	await page.getByTestId("blog-title").waitFor();
+exports.createBlog = async (page, { title, author, url }) => {
+	try {
+		await page.getByRole("button", { name: "add blog" }).click();
+		await page.getByTestId("title").fill(title);
+		await page.getByTestId("author").fill(author);
+		await page.getByTestId("url").fill(url);
+		await page.getByRole("button", { name: "create" }).click();
+
+		await page.getByTestId("blog-title").and(page.getByText(title)).waitFor();
+	} catch (error) {
+		console.error("Error in createBlog:", error);
+		throw error;
+	}
 };
