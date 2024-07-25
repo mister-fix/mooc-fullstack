77b07e37a0a434256a5ae81dad90f1adf6e78cfb
Stephen <hellostephenwm@gmail.com>
2024-07-25 17:07:22 -0400

    reformatted file and refactored code

---
+++ part5/bloglist-frontend/src/components/LoginForm.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import PropTypes from 'prop-types'
+
+const LoginForm = ({ handleLogin }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    handleLogin(username, password)
+    setUsername('')
+    setPassword('')
+  }
+
+  return (
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+					username
+          <input
+            data-testid="username"
+            type="text"
+            name="Username"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+					password
+          <input
+            data-testid="password"
+            type="password"
+            name="Password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+}
+
+LoginForm.propTypes = {
+  handleLogin: PropTypes.func.isRequired,
+}
+
+export default LoginForm
