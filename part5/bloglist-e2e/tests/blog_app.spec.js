b1eb1a1d96a6d450521754d469140bfe2800090d
Stephen <hellostephenwm@gmail.com>
2024-07-24 22:20:46 -0400

    up to exercise 5.20

--- part5/bloglist-e2e/tests/blog_app.spec.js
+++ part5/bloglist-e2e/tests/blog_app.spec.js
@@ -6,9 +6,9 @@ describe("Blog app", () => {
 		await request.post("/api/testing/reset");
 		await request.post("/api/users", {
 			data: {
-				name: "Matti Luukkainen",
-				username: "mluukkai",
-				password: "salainen",
+				name: "Test User",
+				username: "testuser",
+				password: "password",
 			},
 		});
 
@@ -16,45 +16,70 @@ describe("Blog app", () => {
 	});
 
 	test("Login form is shown", async ({ page }) => {
-		await expect(page.getByText("log in to application")).toBeVisible();
-		await expect(page.getByTestId("username")).toBeVisible();
-		await expect(page.getByTestId("password")).toBeVisible();
-		await expect(page.getByRole("button")).toBeVisible();
+		const locator = page.getByText("log in to application");
+		await expect(locator).toBeVisible();
 	});
 
 	describe("Login", () => {
-		test("succeeds with correct credentials", async ({ page }) => {
-			await loginWith(page, "mluukkai", "salainen");
-			await expect(page.getByText("Matti Luukkainen logged in")).toBeVisible();
+		test("Succeeds with correct credentials", async ({ page }) => {
+			await loginWith(page, "testuser", "password");
+			await expect(page.getByText("Test User logged in")).toBeVisible();
 		});
 
-		test("fails with wrong credentials", async ({ page }) => {
-			await loginWith(page, "mluukkai", "wrongpassword");
+		test("Fails with wrong credentials", async ({ page }) => {
+			await loginWith(page, "testuser", "wrongpassword");
 
-			const notificationDiv = await page.locator(".notification");
+			const notificationDiv = page.locator(".notification");
 			await expect(notificationDiv).toContainText("wrong username or password");
-			await expect(notificationDiv).toHaveCSS("border-style", "solid");
-			await expect(notificationDiv).toHaveCSS("color", "rgb(255, 0, 0)");
-			await expect(
-				page.getByText("Martin Luukkainen logged in")
-			).not.toBeVisible();
+			await expect(page.getByText("Test User logged in")).not.toBeVisible();
 		});
 
 		describe("When logged in", () => {
 			beforeEach(async ({ page }) => {
-				await loginWith(page, "mluukkai", "salainen");
+				await loginWith(page, "testuser", "password");
 			});
 
-			test("a new blog can be created", async ({ page }) => {
-				const newBlog = {
+			test("A new blog can be created", async ({ page }) => {
+				await createBlog(page, {
 					title: "A blog created by playwright",
-					author: "John Doe",
-					url: "https://freecodecamp.org",
-				};
-				await createBlog(page, newBlog, true);
+					author: "Play Wright",
+					url: "https://playwright.dev/docs/",
+				});
 
-				const blogTitle = await page.getByTestId("blog-title");
-				await expect(blogTitle).toContainText("A blog created by playwright");
+				const blog = page
+					.getByTestId("blog-title")
+					.and(page.getByText("A blog created by playwright"));
+
+				await expect(blog).toBeVisible();
+			});
+
+			describe("And a blog exists", () => {
+				beforeEach(async ({ page }) => {
+					await createBlog(page, {
+						title: "Test blog",
+						author: "John Doe",
+						url: "http://google.com",
+					});
+				});
+
+				test("A blog can be liked", async ({ page }) => {
+					await page.getByRole("button", { name: "view" }).click();
+					await page.getByRole("button", { name: "like" }).click();
+					await expect(
+						page.getByTestId("blog-likes").and(page.getByText("likes 1"))
+					).toBeVisible();
+				});
+
+				test("A blog can be removed", async ({ page }) => {
+					const targetBlog = page.getByText("Test blog");
+					const targetBlogElement = await targetBlog.locator("..");
+
+					await targetBlogElement.getByRole("button", { name: "view" }).click();
+					await targetBlogElement
+						.getByRole("button", { name: "remove" })
+						.nth(1)
+						.click();
+				});
 			});
 		});
 	});
