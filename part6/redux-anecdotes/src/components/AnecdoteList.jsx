2eb639fbe5f3506b5a9e7076da29d2be19cfbcb1
Stephen <hellostephenwm@gmail.com>
2024-10-02 14:12:43 -0400

    6.11: updated the anecdote rendering to ensure they're sorted and filtered after createSlice updates to reducer

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -17,10 +17,22 @@ const AnecdoteList = () => {
 	const dispatch = useDispatch();
 	const anecdotes = useSelector((state) => {
 		const filter = state.filter.toLowerCase();
+		const anecdotes = state.anecdotes;
+		// Sorting the anecdotes by votes
+		const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes);
 
-		return state.anecdotes.filter((anecdote) =>
-			anecdote.content.toLowerCase().includes(filter)
-		);
+		// Return sorted anecdotes with or without filter applied
+		return filter === ""
+			? sortedAnecdotes
+			: sortedAnecdotes.filter((anecdote) =>
+					anecdote.content.toLowerCase().includes(filter)
+				);
+
+		// return filter === ""
+		// 	? [...state.anecdotes].sort((a, b) => b.votes - a.votes)
+		// 	: state.anecdotes.filter((anecdote) =>
+		// 			anecdote.content.toLowerCase().includes(filter)
+		// 		);
 	});
 
 	return (
