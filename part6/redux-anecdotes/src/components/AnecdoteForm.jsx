d8121546acb77419359240e2c51d54abaeec8a09
Stephen <hellostephenwm@gmail.com>
2024-10-03 13:05:54 -0400

    6.15: used anecdote service to push new anecdotes to backend, modifed createAnecdote dispatch

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -4,17 +4,19 @@ import {
 	clearNotification,
 	setNotification,
 } from "../reducers/notificationReducer";
+import anecdoteService from "../services/anecdotes";
 
 const AnecdoteForm = () => {
 	const dispatch = useDispatch();
 
-	const addAnecdote = (e) => {
+	const addAnecdote = async (e) => {
 		e.preventDefault();
 
 		const content = e.target.anecdote.value;
 		e.target.anecdote.value = "";
 
-		dispatch(createAnecdote(content));
+		const newAnecdote = await anecdoteService.createNew(content);
+		dispatch(createAnecdote(newAnecdote));
 		dispatch(setNotification(`added anecdote: ${content}`));
 
 		setTimeout(() => {
