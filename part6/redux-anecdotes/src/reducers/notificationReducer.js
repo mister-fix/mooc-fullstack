cb0f10b0ba4c161d012e6236b6c0815d68778fd1
Stephen <hellostephenwm@gmail.com>
2024-10-03 15:37:09 -0400

    6.19: renamed slice 'setNotification' reducer to 'showNotification', add async 'setNotification' thunk to handle displaying and clearing notifications

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -2,9 +2,9 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const notificationSlice = createSlice({
 	name: "notification",
-	initialState: "",
+	initialState: null, // No notification by default
 	reducers: {
-		setNotification(state, action) {
+		showNotification(state, action) {
 			return action.payload;
 		},
 		clearNotification() {
@@ -13,6 +13,19 @@ const notificationSlice = createSlice({
 	},
 });
 
-export const { setNotification, clearNotification } = notificationSlice.actions;
+export const { showNotification, clearNotification } =
+	notificationSlice.actions;
+
+export const setNotification = (message, duration) => {
+	return async (dispatch) => {
+		// Dispatch the action to set the notification message
+		dispatch(showNotification(message));
+
+		// Set a timeout to clear the notification after 'duration' seconds
+		setTimeout(() => {
+			dispatch(clearNotification());
+		}, duration * 1000); //Convert seconds to milliseconds
+	};
+};
 
 export default notificationSlice.reducer;
