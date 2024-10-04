bd5e78a072e076cc52253f902bfa70221b66f018
Stephen <hellostephenwm@gmail.com>
2024-10-04 17:09:51 -0400

    6.20: fetched notes from backend with react query

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,40 +1,48 @@
-import AnecdoteForm from './components/AnecdoteForm'
-import Notification from './components/Notification'
+import { useQuery } from "@tanstack/react-query";
+import { getAnecdotes } from "./services/anecdotes";
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
 
 const App = () => {
+	const handleVote = (anecdote) => {
+		console.log("vote");
+	};
 
-  const handleVote = (anecdote) => {
-    console.log('vote')
-  }
+	const { isPending, isError, data, error } = useQuery({
+		queryKey: ["anecdotes"],
+		queryFn: getAnecdotes,
+		retry: false,
+	});
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+	console.log("anecdotes:", data);
 
-  return (
-    <div>
-      <h3>Anecdote app</h3>
-    
-      <Notification />
-      <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
-          </div>
-        </div>
-      )}
-    </div>
-  )
-}
+	if (isPending) {
+		return <div>loading data...</div>;
+	}
 
-export default App
+	if (isError) {
+		console.error("Error:", error.message);
+		return <div>anecdote service not available due to problems in server</div>;
+	}
+
+	return (
+		<div>
+			<h3>Anecdote app</h3>
+
+			<Notification />
+			<AnecdoteForm />
+
+			{data.map((anecdote) => (
+				<div key={anecdote.id}>
+					<div>{anecdote.content}</div>
+					<div>
+						has {anecdote.votes}
+						<button onClick={() => handleVote(anecdote)}>vote</button>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default App;
