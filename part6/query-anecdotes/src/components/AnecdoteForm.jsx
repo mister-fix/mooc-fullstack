b13b5d1c6d54528aedbd188117f7b4b1cddd0669
Stephen <hellostephenwm@gmail.com>
2024-10-04 17:25:39 -0400

    6.21: add anecdote creating mutation to save new anecdotes to backend

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,21 +1,38 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createAnecdote } from "../services/anecdotes";
+
 const AnecdoteForm = () => {
+	// Initialized 'useQueryClient' function
+	const queryClient = useQueryClient();
 
-  const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+	// New anecdote mutation: saves new anecdote to backend and ensures it's rendered in frontend
+	const newAnecdoteMutation = useMutation({
+		mutationFn: createAnecdote,
+		onSuccess: (newAnecdote) => {
+			const anecdotes = queryClient.getQueryData(["anecdotes"]);
+			queryClient.setQueryData(["anecdotes"], anecdotes.concat(newAnecdote));
+			// queryClient.invalidateQueries({ queryKey: ["anecdotes"] });
+		},
+	});
 
-  return (
-    <div>
-      <h3>create new</h3>
-      <form onSubmit={onCreate}>
-        <input name='anecdote' />
-        <button type="submit">create</button>
-      </form>
-    </div>
-  )
-}
+	const onCreate = (event) => {
+		event.preventDefault();
+		const content = event.target.anecdote.value;
+		event.target.anecdote.value = "";
+		console.log("new anecdote:", content);
+		// Calling new anecdote mutation
+		newAnecdoteMutation.mutate({ content, votes: 0 });
+	};
 
-export default AnecdoteForm
+	return (
+		<div>
+			<h3>create new</h3>
+			<form onSubmit={onCreate}>
+				<input name="anecdote" />
+				<button type="submit">create</button>
+			</form>
+		</div>
+	);
+};
+
+export default AnecdoteForm;
