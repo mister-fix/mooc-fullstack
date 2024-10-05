f914b06e6a99e8131ebb864a3a84382816c9a202
Stephen <hellostephenwm@gmail.com>
2024-10-05 16:33:37 -0400

    6.23: employed custom 'useNotification' hook to display notification when new anecdotes are added

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,9 +1,11 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createAnecdote } from "../services/anecdotes";
+import { useNotification } from "../NotificationContext";
 
 const AnecdoteForm = () => {
 	// Initialized 'useQueryClient' function
 	const queryClient = useQueryClient();
+	const { showNotification } = useNotification();
 
 	// New anecdote mutation: saves new anecdote to backend and ensures it's rendered in frontend
 	const newAnecdoteMutation = useMutation({
@@ -12,6 +14,7 @@ const AnecdoteForm = () => {
 			const anecdotes = queryClient.getQueryData(["anecdotes"]);
 			queryClient.setQueryData(["anecdotes"], anecdotes.concat(newAnecdote));
 			// queryClient.invalidateQueries({ queryKey: ["anecdotes"] });
+			showNotification(`added anecdote: '${newAnecdote.content}'`, 5);
 		},
 	});
 
@@ -26,6 +29,7 @@ const AnecdoteForm = () => {
 
 	return (
 		<div>
+			{/* <Notification /> */}
 			<h3>create new</h3>
 			<form onSubmit={onCreate}>
 				<input name="anecdote" />
