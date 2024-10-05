001dff81233e902fc5c5e7a039b6746e73a999ab
Stephen <hellostephenwm@gmail.com>
2024-10-05 16:53:05 -0400

    6.24: expanded component to display anecdote creation error from server

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -3,33 +3,31 @@ import { createAnecdote } from "../services/anecdotes";
 import { useNotification } from "../NotificationContext";
 
 const AnecdoteForm = () => {
-	// Initialized 'useQueryClient' function
 	const queryClient = useQueryClient();
 	const { showNotification } = useNotification();
 
-	// New anecdote mutation: saves new anecdote to backend and ensures it's rendered in frontend
 	const newAnecdoteMutation = useMutation({
 		mutationFn: createAnecdote,
 		onSuccess: (newAnecdote) => {
 			const anecdotes = queryClient.getQueryData(["anecdotes"]);
 			queryClient.setQueryData(["anecdotes"], anecdotes.concat(newAnecdote));
-			// queryClient.invalidateQueries({ queryKey: ["anecdotes"] });
-			showNotification(`added anecdote: '${newAnecdote.content}'`, 5);
+			showNotification(`added anecdote: '${newAnecdote.content}'`, 5); // Displaying added anecdote notification
+		},
+		onError: (error) => {
+			showNotification(error.message, 5); // Displaying error message from server
 		},
 	});
 
 	const onCreate = (event) => {
 		event.preventDefault();
 		const content = event.target.anecdote.value;
-		event.target.anecdote.value = "";
-		console.log("new anecdote:", content);
-		// Calling new anecdote mutation
+
 		newAnecdoteMutation.mutate({ content, votes: 0 });
+		event.target.anecdote.value = ""; // Clear input field on successful submission
 	};
 
 	return (
 		<div>
-			{/* <Notification /> */}
 			<h3>create new</h3>
 			<form onSubmit={onCreate}>
 				<input name="anecdote" />
