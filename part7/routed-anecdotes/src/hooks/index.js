8cccdda7b0416c5c6d94196ee10a112de8f7d798
Stephen <hellostephenwm@gmail.com>
2024-10-17 18:57:26 -0400

    feat: useField hook that works with form fields

---
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+/**
+ * @function useField
+ * @description Custom hook that works with form fields to set/track the value (or input) of the field.
+ * @param {*} type - Form field type (text, date, etc.)
+ * @constant
+ * @returns  {Object} Object containing the form field type, its inputted value, and onChange handler.
+ */
+export const useField = (type) => {
+	// Initializing the state of the value to an empty string
+	const [value, setValue] = useState("");
+
+	// onChange handling function for when the field receives an input
+	const onChange = (e) => {
+		setValue(e.target.value);
+	};
+
+	// Returning the field type, value, and onChange handler
+	return {
+		type,
+		value,
+		onChange,
+	};
+};
