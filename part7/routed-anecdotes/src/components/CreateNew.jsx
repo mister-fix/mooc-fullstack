50f3eff177c296b206a50c695b0791bf5d3ba833
Stephen <hellostephenwm@gmail.com>
2024-10-17 18:58:05 -0400

    feat: employed custom useField hook to refactor code

--- part7/routed-anecdotes/src/components/CreateNew.jsx
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -1,18 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useField } from "../hooks";
 
 const CreateNew = (props) => {
-	const [content, setContent] = useState("");
-	const [author, setAuthor] = useState("");
-	const [info, setInfo] = useState("");
+	const content = useField("text");
+	const author = useField("text");
+	const info = useField("text");
 	const navigate = useNavigate();
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		props.addNew({
-			content,
-			author,
-			info,
+			content: content.value,
+			author: author.value,
+			info: info.value,
 			votes: 0,
 		});
 		navigate("/");
@@ -26,24 +27,24 @@ const CreateNew = (props) => {
 					content
 					<input
 						name="content"
-						value={content}
-						onChange={(e) => setContent(e.target.value)}
+						value={content.value}
+						onChange={content.onChange}
 					/>
 				</div>
 				<div>
 					author
 					<input
 						name="author"
-						value={author}
-						onChange={(e) => setAuthor(e.target.value)}
+						value={author.value}
+						onChange={author.onChange}
 					/>
 				</div>
 				<div>
 					url for more info
 					<input
 						name="info"
-						value={info}
-						onChange={(e) => setInfo(e.target.value)}
+						value={info.value}
+						onChange={info.onChange}
 					/>
 				</div>
 				<button>create</button>
