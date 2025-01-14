bee182ed734307c584fc339170d40672a2d2a69f
Stephen <hellostephenwm@gmail.com>
2025-01-14 17:31:45 -0500

    7.10 => chore: reformat component to utilize state from redux

--- part7/redux-bloglist/src/components/Notification.jsx
+++ part7/redux-bloglist/src/components/Notification.jsx
@@ -1,15 +1,21 @@
-const Notification = ({ message, type }) => {
-  if (message === null) {
-    return null;
-  }
+import { useSelector } from 'react-redux';
 
-  return type === 'success' ? (
-    <div className="notification notification-success">{message}</div>
-  ) : type === 'warning' ? (
-    <div className="notification notification-warning">{message}</div>
-  ) : (
-    <div className="notification notification-error">{message}</div>
-  );
+const Notification = () => {
+  const notification = useSelector((state) => state.notification);
+
+  if (!notification) return null;
+
+  const style = {
+    border: '1px solid #0A3622',
+    color: '#0A3622',
+    backgroundColor: '#D1E7DD',
+    marginBottom: 20,
+    fontSize: 16,
+    padding: 12,
+    borderRadius: 8,
+  };
+
+  return <div style={style}>{notification}</div>;
 };
 
 export default Notification;
