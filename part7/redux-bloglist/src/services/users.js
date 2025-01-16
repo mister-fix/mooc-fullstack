e80c6531db8c0263bb48897f4c2ac90e5f8ed2f2
Stephen <hellostephenwm@gmail.com>
2025-01-16 15:24:12 -0500

    7.14 => feat: fetch single user

---
+++ part7/redux-bloglist/src/services/users.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const baseUrl = '/api/users';
+
+let token = null;
+
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`;
+};
+
+const getAll = async () => {
+  const config = {
+    headers: { Authorization: token },
+  };
+  const response = await axios.get(baseUrl, config);
+  return response.data;
+};
+
+const getOne = async (id) => {
+  const config = {
+    headers: { Authorization: token },
+  };
+  const response = await axios.get(`${baseUrl}/${id}`, config);
+  return response.data;
+};
+
+export default { getAll, getOne };
