89618aba5e2051a26cbaa3f81655d188e4870afc
Stephen <hellostephenwm@gmail.com>
2025-01-16 20:36:45 -0500

    7.18 => chore: removed unused code

--- part7/redux-bloglist/src/components/Blog.jsx
+++ part7/redux-bloglist/src/components/Blog.jsx
@@ -1,28 +1,10 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const Blog = ({ blog, handleLike, handleDelete }) => {
-  const [visibility, setVisibility] = useState(false);
-  const showWhenVisible = { display: visibility ? '' : 'none' };
+const Blog = ({ blog }) => {
   const blogStyle = {
     padding: '10px 0 0 2px',
     border: '1px solid black',
     marginBottom: 5,
-  };
-
-  const toggleVisibility = () => {
-    setVisibility(!visibility);
-  };
-
-  const matchUser = () => {
-    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser');
-
-    if (loggedUserJSON && blog.user) {
-      const { username } = JSON.parse(loggedUserJSON);
-      return username === blog.user.username;
-    }
-
-    return false;
   };
 
   return (
