713f322c2ecb9411338fe6858c535285d80c6b1d
Stephen <hellostephenwm@gmail.com>
2025-01-22 23:55:24 -0500

    7.11 => chore: wrapped application in query provider

--- part7/query-bloglist/src/main.jsx
+++ part7/query-bloglist/src/main.jsx
@@ -1,13 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import NotificationProvider from "./providers/NotificationContext";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <NotificationProvider>
-      <App />
-    </NotificationProvider>
+    <QueryClientProvider client={queryClient}>
+      <NotificationProvider>
+        <App />
+      </NotificationProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
