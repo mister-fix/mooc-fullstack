6ca346b0f92d18f8db28227aae265d12934b7f70
Stephen <hellostephenwm@gmail.com>
2025-01-24 12:22:15 -0500

    7.14 => chore: enabled react router's browser router

--- part7/query-bloglist/src/main.jsx
+++ part7/query-bloglist/src/main.jsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import NotificationProvider from "./providers/NotificationContext";
@@ -10,12 +11,14 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <UserProvider>
-        <NotificationProvider>
-          <App />
-        </NotificationProvider>
-      </UserProvider>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <UserProvider>
+          <NotificationProvider>
+            <App />
+          </NotificationProvider>
+        </UserProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
