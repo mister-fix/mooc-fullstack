c6066e1810c7be784de89d8bc41489552935291c
Stephen <hellostephenwm@gmail.com>
2025-01-24 14:27:48 -0500

    7.17 => chore: reformatted component

--- part7/query-bloglist/src/components/Blog.jsx
+++ part7/query-bloglist/src/components/Blog.jsx
@@ -1,46 +1,9 @@
-import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const Blog = ({ blog, handleLike, handleDelete }) => {
-  const [visibility, setVisibility] = useState(false);
-  const showWhenVisible = { display: visibility ? "" : "none" };
-
-  const toggleVisibility = () => {
-    setVisibility(!visibility);
-  };
-
-  const matchUser = () => {
-    const loggedUserJSON = window.localStorage.getItem("loggedBlogAppUser");
-
-    if (loggedUserJSON && blog.user) {
-      const { username } = JSON.parse(loggedUserJSON);
-      return username === blog.user.username;
-    }
-
-    return false;
-  };
-
+const Blog = ({ blog }) => {
   return (
     <div className="blog">
-      <p className="blog-title">{blog.title}</p>
-      <p className="blog-author">{blog.author}</p>
-      <button onClick={toggleVisibility}>{visibility ? "hide" : "show"}</button>
-
-      <div style={showWhenVisible}>
-        <p className="blog-url">
-          <a href="#" target="_blank" rel="noopener noreferrer">
-            {blog.url}
-          </a>
-        </p>
-        <p className="blog-likes">
-          likes {blog.likes}{" "}
-          <button onClick={(event) => handleLike(event, blog.id)}>like</button>
-        </p>
-        {matchUser() && (
-          <button onClick={(event) => handleDelete(event, blog.id)}>
-            remove
-          </button>
-        )}
-      </div>
+      <Link to={`/blogs/${blog.id}`}>{blog.title}</Link>
     </div>
   );
 };
