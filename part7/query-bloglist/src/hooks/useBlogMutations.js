b41afb20db157d9f4c21d544c4e395c4519959b1
Stephen <hellostephenwm@gmail.com>
2025-01-23 19:38:56 -0500

    7.12 => chore: defined blog liking and deleting mutations

--- part7/query-bloglist/src/hooks/useBlogMutations.js
+++ part7/query-bloglist/src/hooks/useBlogMutations.js
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createBlog } from "../services/blogs";
+import { createBlog, removeBlog, updateBlog } from "../services/blogs";
 
 export const useCreateBlog = () => {
   const queryClient = useQueryClient();
@@ -12,6 +12,41 @@ export const useCreateBlog = () => {
     },
     onError: (error) => {
       console.error("Error creating blog:", error);
+    },
+  });
+};
+
+export const useLikeBlog = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async ({ id, updatedBlog }) => updateBlog(id, updatedBlog),
+    onSuccess: (updatedBlog) => {
+      queryClient.setQueryData(["blogs"], (oldBlogs) =>
+        oldBlogs.map((blog) =>
+          blog.id === updatedBlog.id ? updatedBlog : blog,
+        ),
+      );
+    },
+    onError: (error) => {
+      console.error("Error liking blog:", error);
+    },
+  });
+};
+
+export const useDeleteBlog = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: removeBlog,
+    onSuccess: (_, id) => {
+      // Use underscore instead of data since it will go unused
+      queryClient.setQueryData(["blogs"], (oldBlogs) => {
+        oldBlogs.filter((blog) => blog.id !== id);
+      });
+    },
+    onError: (error) => {
+      console.error("Error deleting blog:", error);
     },
   });
 };
