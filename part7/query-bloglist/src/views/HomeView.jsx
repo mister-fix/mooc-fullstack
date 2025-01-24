80c2f04a8776cfd03dcd5ed6a0eda0468f1d4ffa
Stephen <hellostephenwm@gmail.com>
2025-01-24 14:28:46 -0500

    7.16 => chore: reformatted component

--- part7/query-bloglist/src/views/HomeView.jsx
+++ part7/query-bloglist/src/views/HomeView.jsx
@@ -2,19 +2,13 @@ import { useRef } from "react";
 import Blog from "../components/Blog";
 import BlogForm from "../components/BlogForm";
 import Togglable from "../components/Togglable";
-import {
-  useCreateBlog,
-  useDeleteBlog,
-  useLikeBlog,
-} from "../hooks/useBlogMutations";
+import { useCreateBlog } from "../hooks/useBlogMutations";
 import { useNotification } from "../providers/NotificationContext";
 
 const HomeView = ({ user, isPending, blogs }) => {
   const blogFormRef = useRef();
-  const { mutate: createBlog } = useCreateBlog();
-  const { mutate: likeBlog } = useLikeBlog();
-  const { mutate: deleteBlog } = useDeleteBlog();
   const { showNotification } = useNotification();
+  const { mutate: createBlog } = useCreateBlog();
 
   const addBlog = async (blogObject) => {
     blogFormRef.current.toggleVisibility();
@@ -45,40 +39,6 @@ const HomeView = ({ user, isPending, blogs }) => {
     });
   };
 
-  const handleLike = async (event, id) => {
-    event.preventDefault();
-
-    const blog = blogs.find((b) => b.id === id);
-
-    likeBlog(
-      { id, updatedBlog: { ...blog, likes: blog.likes + 1 } },
-      {
-        onError: () => {
-          showNotification("Error liking blog", 5);
-        },
-      },
-    );
-  };
-
-  const handleDelete = async (event, id) => {
-    event.preventDefault();
-
-    const blog = blogs.find((b) => b.id === id);
-    const { title, author } = blog;
-    const accept = window.confirm(`Delete blog "${title}" by ${author}?`);
-
-    if (accept) {
-      deleteBlog(id, {
-        onSuccess: () => {
-          showNotification(`Blog "${title}: by ${author} has been removed.`, 5);
-        },
-        onError: () => {
-          showNotification("Error removing blog", 5);
-        },
-      });
-    }
-  };
-
   return (
     <div>
       <Togglable buttonLabel={"add blog"} ref={blogFormRef}>
@@ -92,12 +52,7 @@ const HomeView = ({ user, isPending, blogs }) => {
           {blogs
             .sort((a, b) => b.likes - a.likes)
             .map((blog) => (
-              <Blog
-                key={blog.id}
-                blog={blog}
-                handleLike={handleLike}
-                handleDelete={handleDelete}
-              />
+              <Blog key={blog.id} blog={blog} />
             ))}
         </div>
       )}
